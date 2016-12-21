af0e490cfcb4f056e4420f6c47509a63ed1d0014
Tim Davis <timdavish@gmail.com>
2016-12-20 18:22:48 -0800

    Beanstalk deploy attempt, angular switches added, calendar added

--- public/javascripts/t3.js
+++ public/javascripts/t3.js
@@ -1,25 +1,32 @@
 
 // Define our app
-var app = angular.module('T3', ['ngResource', 'ngRoute']);
+var app = angular.module('T3', []);
 
 // Configure the route provider
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'partials/index.html',
-            controller: 'HomeCtrl'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-}]);
+// app.config(['$routeProvider', function($routeProvider) {
+//     $routeProvider
+//         .when('/', {
+//             templateUrl: 'partials/index.html',
+//             controller: 'HomeCtrl'
+//         })
+//         .otherwise({
+//             redirectTo: '/'
+//         });
+// }]);
 
 // Home controller
-app.controller('HomeCtrl', ['$scope', '$resource',
-    function($scope, $resource) {
-        var Courses = $resource('/courses');
-        Courses.query(function(courses) {
-            $scope.courses = courses;
-        });
-    }
-]);
+// app.controller('HomeCtrl', ['$scope', '$resource',
+//     function($scope, $resource) {
+//         var Courses = $resource('/courses');
+//         Courses.query(function(courses) {
+//             $scope.courses = courses;
+//         });
+//     }
+// ]);
+
+// Header controller (index.jade)
+app.controller('headerCtrl', function($scope) {
+    $scope.view = {
+        name: ''
+    };
+});
